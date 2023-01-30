100c6dff7222a113b0c255f11478e0907f5f39e2
syon1203 <syon1203@me.com>
2023-01-30 17:50:09 +0900

    resnet50 wo DDP

--- resnet50_classification/main.py
+++ resnet50_classification/main.py
@@ -9,44 +9,36 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group #initialize
 
 from torch.utils.data import Dataset, DataLoader
+from torchvision.datasets import ImageFolder
 import argparse
 
 import torchvision.transforms as tr
 #import util
-from dataset import Imagenet
 
 import matplotlib.pyplot as plt
 from torchvision.utils import make_grid
-
-from torchvision.datasets import ImageFolder
 
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-
-
 def ddp_setup(rank, world_size):
     #world size == total num of processes in a group
     #rank == identifier assigned to each process
 
-    os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = ""
-    init_process_group(backend="nccl",rank=rank, world_size=world_size)
+    os.environ["MASTER_ADDR"] = "115.145.134.134"
+    os.environ["MASTER_PORT"] = "22"
 
+    init_process_group(backend="gloo",rank=rank, world_size=world_size)
 
+def cleanup():
+    destroy_process_group()
 
 # GPU Device 
+#gpu_id = '2'
+#os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id) 
 
-gpu_id = '2' 
 
-os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id) 
-
-use_cuda = torch.cuda.is_available() 
-
-print("GPU device " , use_cuda) 
-
-device = torch.device('cuda' if use_cuda else 'cpu')
 
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -57,17 +49,31 @@ import matplotlib.pyplot as plt
 # plt.imshow(np.asarray(img))
 # plt.show()
 
-
-def main():
-    parser = argparse.ArgumentParser(description='argparse')
+parser = argparse.ArgumentParser(description='argparse')
 
     # dataset, model, batch size, epoch, learning rate
-    parser.add_argument('--train', '-tr', required=False, default='/home/data/Imagenet/train', help='Root of Trainset')
-    parser.add_argument('--test', '-ts', required=False, default='/home/data/Imagenet/validation', help='Root of Testset')
-    parser.add_argument('--model', '-m', required=False, default='resnet50', help='Model')
-    parser.add_argument('--batch', '-b', required=False, default=32, help='Batch Size')
-    parser.add_argument('--epoch', '-e', required=False, default=50, help='Epoch')
-    parser.add_argument('--lr', '-l', required=False, default=0.001, help='Learning Rate')
+parser.add_argument('--train', '-tr', required=False, default='/home/data/Imagenet/train', help='Root of Trainset')
+parser.add_argument('--test', '-ts', required=False, default='/home/data/Imagenet/validation', help='Root of Testset')
+parser.add_argument('--model', '-m', required=False, default='resnet50', help='Model')
+parser.add_argument('--batch', '-b', required=False, default=32, help='Batch Size')
+parser.add_argument('--epoch', '-e', required=False, default=10, help='Epochs')
+parser.add_argument('--lr', '-l', required=False, default=0.1, help='Learning Rate')
+parser.add_argument('--workers', '-w', required=False, default=4, help='Num of data loading workers')
+
+use_cuda = torch.cuda.is_available()
+
+
+def main():
+
+
+    if torch.cuda.is_available():
+            ngpus_per_node = torch.cuda.device_count()
+            print(ngpus_per_node)
+
+    print("GPU device " , use_cuda) 
+
+    device = torch.device('cuda' if use_cuda else 'cpu')
+
 
     args = parser.parse_args()
 
@@ -83,7 +89,7 @@ def main():
     test_root = args.test
 
     if args.model == 'resnet50':
-        model = MD.ResNet50('Imagenet')
+        model = MD.ResNet50()
 
     # Data transforms (normalization & data augmentation)
     stats = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
@@ -92,8 +98,8 @@ def main():
                                tr.RandomHorizontalFlip(), tr.Normalize(*stats, inplace=True)]) #inplace = 변경된 스탯으로 덮어씀
     transf_test = tr.Compose([tr.ToTensor(), tr.Normalize(*stats, inplace=True)])
 
-    trainset = ImageFolder('/home/data/Imagenet/train', transform=transf_train,target_transform=None)
-    testset = ImageFolder('/home/data/Imagenet/validation', transform=transf_test)
+    trainset = ImageFolder(train_root, transform=transf_train,target_transform=None)
+    testset = ImageFolder(test_root, transform=transf_test)
 
     trainloader = DataLoader(trainset, batch_size=args.batch, shuffle=True)
     testloader = DataLoader(testset, batch_size=args.batch, shuffle=False) 
@@ -102,6 +108,11 @@ def main():
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9) #loss 계산시 gradient descent 를 미니배치를 이용함
     scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3, threshold=1e-3)
     #loss 향상하지 않으면 lr 을 factor배로 감소(0.5), 3epoch동안, threhold란 중요변화에만 초점 맞추기 위한 임계값
+
+    #ddp_model = DDP(model, device_ids=["gpu id"])
+
+    
+
 
 if __name__ == "__main__":
     main()
