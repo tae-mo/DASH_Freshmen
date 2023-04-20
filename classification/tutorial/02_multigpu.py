02d304ad297199e112c64e25d9fb8759ef4e8942
KimJwn <kimsharon9@gmail.com>
2023-04-20 23:27:21 +0900

    tutorial 보강

--- classification/tutorial/02_multigpu.py
+++ classification/tutorial/02_multigpu.py
@@ -6,13 +6,25 @@ from torchvision import transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
+'''*Newrly Added For Muti GPU*'''
 import torch.multiprocessing as mp
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 import os
 
+class MyTrainDataset(Dataset):
+    def __init__(self, size):
+        self.size = size
+        self.data = [(torch.rand(20), torch.rand(1)) for _ in range(size)]
 
+    def __len__(self):
+        return self.size
+    
+    def __getitem__(self, index):
+        return self.data[index]
+
+'''*Newrly Added For Muti GPU*'''
 def ddp_setup(rank, world_size):
     """
     Args:
@@ -20,7 +32,7 @@ def ddp_setup(rank, world_size):
         world_size: Total number of processes
     """
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "12355"
+    os.environ["MASTER_PORT"] = "12355"     # free port 
     init_process_group(backend="nccl", rank=rank, world_size=world_size)
 
 class Trainer:
@@ -37,7 +49,9 @@ class Trainer:
         self.train_data = train_data
         self.optimizer = optimizer
         self.save_every = save_every
-        self.model = DDP(model, device_ids=[gpu_id])
+        
+        '''*Newrly Added For Muti GPU*'''
+        self.model = DDP(model, device_ids=[gpu_id]) # single list of gpu id
         self.losses = []
     #
 
@@ -65,6 +79,8 @@ class Trainer:
     #
     
     def _save_checkpoint(self, epoch):
+        
+        '''*Newrly Added For Muti GPU* : model.module로 바꿔준다. '''
         ckp = self.model.module.state_dict()
         PATH = "checkpoint.pt"
         torch.save(ckp, PATH)
@@ -77,6 +93,7 @@ class Trainer:
             
             # loss 값 저장 
             
+            '''*Newrly Added For Muti GPU*'''
             # save_every일 때 마다 체크 포인트 저장 
             if self.gpu_id == 0 and epoch % self.save_every == 0:
                 self._save_checkpoint(epoch)
@@ -84,7 +101,9 @@ class Trainer:
 #
 
 def load_train_objs():
-    
+    train_set = MyTrainDataset(2048)  # load your dataset
+    model = torch.nn.Linear(20, 1)  # load your model
+    '''    
     traindir = os.path.join("/home/data/Imagenet", 'val')
     # valdir = os.path.join(args.data, 'val')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -98,24 +117,14 @@ def load_train_objs():
             transforms.ToTensor(),
             normalize,
         ]))
-    # train_dataset, _ = randomSplit
-    
-    """
-    # val_dataset = datasets.ImageFolder(
-    #     valdir,
-    #     transforms.Compose([
-    #         transforms.Resize(256),
-    #         transforms.CenterCrop(224),
-    #         transforms.ToTensor(),
-    #         normalize,
-    #     ]))"""
-    
     model = models.__dict__['resnet50'](pretrained=True)
+    '''
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-    return train_dataset, model, optimizer
+    return train_set, model, optimizer
 #
 
 def prepare_dataloader(dataset: Dataset, batch_size: int):
+    '''*Newrly Added For Muti GPU* : 샘플러가 추가되었어요 '''
     return DataLoader(
         dataset,
         batch_size=batch_size,
@@ -124,9 +133,10 @@ def prepare_dataloader(dataset: Dataset, batch_size: int):
         sampler=DistributedSampler(dataset)
     )
 
-
+'''*Newrly Added For Muti GPU* : ddp_setup, rank, world_size '''
 def main(rank: int, world_size: int, save_every: int, total_epochs: int, batch_size: int):
     ddp_setup(rank, world_size)
+    
     dataset, model, optimizer = load_train_objs()
     train_data = prepare_dataloader(dataset, batch_size)
     trainer = Trainer(model, train_data, optimizer, rank, save_every)
@@ -142,5 +152,6 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size', default=32, type=int, help='Input batch size on each device (default: 32)')
     args = parser.parse_args()
     
-    world_size = 2 #torch.cuda.device_count()
+    world_size = torch.cuda.device_count()
+    '''*Newrly Added For Muti GPU* : muliprocessing '''
     mp.spawn(main, args=(world_size, args.save_every, args.total_epochs, args.batch_size), nprocs=world_size)
